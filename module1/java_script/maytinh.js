25a0e3378692405123b0f68febd95693084f0016
quoc1405 <quocnguyen1405@gmail.com>
2020-05-08 08:20:48 +0700

    bai tap khai bao bien

---
+++ module1/java_script/maytinh.js
@@ -0,0 +1,23 @@
+function tinhtoan() {
+
+var value1=document.getElementById('1').value;
+var value2=document.getElementById('2').value;
+var value3=document.getElementById('3').value;
+var value4=document.getElementById('4').value;
+var value5=document.getElementById('5').value;
+var value6=document.getElementById('6').value;
+var value7=document.getElementById('7').value;
+var value8=document.getElementById('8').value;
+var value9=document.getElementById('9').value;
+var cong=document.getElementById('cong').value;
+var tru=document.getElementById('tru').value;
+var nhan=document.getElementById('nhan').value;
+var chia=document.getElementById('chia').value;
+var kektqua=document.getElementById('ketqua').value;
+let ketqua = document.getElementById('ketqua').value;
+alert('abc')
+if((typeof ketqua) != 'number')
+{
+    document.getElementById('ketqua')
+}
+}
